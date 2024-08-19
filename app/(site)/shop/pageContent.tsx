6f462a0a0303f6835890ace8cfd596975c6061c1
DGoiana <diogogoianam@gmail.com>
2024-08-20 00:36:57 +0100

    adding reactivity pt2

---
+++ app/(site)/shop/pageContent.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import SectionHeader from "@/components/Common/SectionHeader";
+import ProductPreview from "@/components/Shop/ProductPreview";
+import ShopCard from "@/components/Shop/ShopCard";
+import { Product } from "@/payload-types";
+import React from "react";
+
+export default function ShopPageContent({ products }) {
+  const [previewProduct, setPreviewProduct] = React.useState(products[0]);
+
+  return (
+    <>
+      <div className="mt-25">
+        <SectionHeader
+          headerInfo={{
+            title: "Shop",
+            subtitle: "",
+            description: "",
+          }}
+        ></SectionHeader>
+      </div>
+      <div className="flex flex-row my-5 mx-20">
+        <section className="flex flex-col max-w-[300px]">
+          <ProductPreview product={previewProduct}></ProductPreview>
+        </section>
+        <section className="flex flex-col ml-30">
+          <h1 className="text-7xl font-serif tracking-tight text-black">
+            Student Essentials
+          </h1>
+          <h1 className="text-2xl font-serif text-black mt-5 tracking-tight max-w-96">
+            Os <span className="text-engenharia italic">merch-essentials</span>{" "}
+            deste ano para viveres a tua vida académica ao máximo, com estilo.
+          </h1>
+          <div>
+            <div className="mt-10 flex gap-2">
+              {products.map((product: Product) => (
+                <ShopCard
+                  product={product}
+                  key={product.id}
+                  customClick={() => setPreviewProduct(product)}
+                ></ShopCard>
+              ))}
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
