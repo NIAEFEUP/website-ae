5f6ab5d6260d4ed7fde50eeabe9188c0fca39636
DGoiana <diogogoianam@gmail.com>
2024-10-01 00:14:34 +0100

    state in the cart

--- components/Shop/Cart/index.tsx
+++ components/Shop/Cart/index.tsx
@@ -14,17 +14,18 @@ type ShopCartProps = {
    isOpen: boolean,
    onOpenChange: (bool: boolean) => void,
    products: Product[],
+   removeFromCart: (productID: number) => void,
 }
 
-const ShopCart = ({ isOpen, onOpenChange, products }: ShopCartProps) => {
+const ShopCart = ({ isOpen, onOpenChange, products, removeFromCart }: ShopCartProps) => {
    return <Sheet open={isOpen} onOpenChange={onOpenChange}>
       <SheetContent className="z-100000 flex flex-col justify-between">
          <SheetHeader>
-            <SheetTitle>Cart</SheetTitle>
+            <SheetTitle className="mb-5">Cart</SheetTitle>
             <SheetDescription className="flex flex-col gap-5">
                {products.map((p) => {
                   return <>
-                     <CartItem product={p}></CartItem>
+                     <CartItem product={p} removeFromCart={removeFromCart}></CartItem>
                      <hr></hr>
                   </>
                })}
