555d3efebd47216cf1e417d8d11d20558ffd5c67
Vitor Pires <vitormanuelpp@gmail.com>
2024-10-02 22:47:12 +0100

    feat: footer

--- components/Sponsors/index.tsx
+++ components/Sponsors/index.tsx
@@ -11,7 +11,7 @@ interface Props {
 
 const Sponsors = ({ sponsors }: Props) => {
   return (
-    <section className="border-t bg-alabaster py-11 dark:border-y-strokedark dark:bg-black">
+    <section className="border-t bg-alabaster py-7 dark:border-y-strokedark dark:bg-black">
       <div className="mx-auto max-w-c-1390 px-4 md:px-8 2xl:px-0">
         <div className="grid grid-cols-3 items-center justify-center gap-7.5 md:grid-cols-6 lg:gap-12.5 xl:gap-29">
           {sponsors.map((sponsor, index) => {
