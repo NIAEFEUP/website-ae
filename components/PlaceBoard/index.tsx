0ac4bd175246d37b6f71de97efa20bc91caa42b8
Vitor Pires <vitormanuelpp@gmail.com>
2024-11-15 01:15:43 +0000

    feat: schedule table

--- components/PlaceBoard/index.tsx
+++ components/PlaceBoard/index.tsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Place } from '@/payload-types';
 import { motion } from "framer-motion";
+import ScheduleTable from "../Table";
 
 interface Props {
   places: Place[];
@@ -14,17 +15,17 @@ const PlaceBoard = ({ places, selected, onChange }: Props) => {
     <>
       <section id="features" className="flex justify-center gap-5 flex-wrap mx-5">
         {places.map((place, key) => (
-          <motion.button
+            <motion.button
               key={place.id}
               variants={{
-                hidden: {
-                  opacity: 0,
-                  y: -10,
-                },
-                visible: {
-                  opacity: 1,
-                  y: 0,
-                },
+              hidden: {
+                opacity: 0,
+                y: -10,
+              },
+              visible: {
+                opacity: 1,
+                y: 0,
+              },
               }}
               initial="hidden"
               whileInView="visible"
@@ -34,18 +35,17 @@ const PlaceBoard = ({ places, selected, onChange }: Props) => {
               className={`w-1/4 min-w-90 p-2 animate_top z-40 rounded-lg border border-white dark:border-strokedark shadow-solid-3 transition-all hover:shadow-solid-4 dark:hover:bg-hoverdark dark:bg-blacksection ${place.id === selected ? "bg-primary opacity-40" : "bg-white"}`}
             >
               <h3 className={`text-base font-semibold ${place.id === selected ? "text-white" : "text-black dark:text-white"}  xl:text-itemtitle`}>
-                {place.name}
+              {place.name}
               </h3>
               {place.description && (
-                <p className={`${place.id === selected ? "text-white" : "text-black dark:text-white"}`}>
-                  {place.description}
-                </p>
+              <p className={`${place.id === selected ? "text-white" : "text-black dark:text-white"}`}>
+                {place.description}
+              </p>
+                )}
+                {place.schedule && (
+                <ScheduleTable data={place.schedule.map(slot => ({ label: slot.day, value: slot.hours }))} />
               )}
-              {place.schedule?.map((slot) => (
-                <p className={`${place.id === selected ? "text-white" : "text-black dark:text-white"}`}>{slot.day}: {slot.hours}</p>
-              )
-            )}
-          </motion.button>
+            </motion.button>
         ))}
       </section>
     </>
