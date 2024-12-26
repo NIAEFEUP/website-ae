9532ed230397b2f73f5f24fb97221c6595d1a911
Vitor Pires <vitormanuelpp@gmail.com>
2024-12-26 23:37:19 +0000

    fix: error in header

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -36,7 +36,7 @@ const Header = () => {
 
   return (
     <header
-      className={`z-99999 fixed left-0 top-0 z-50 w-full py-4 ${stickyMenu
+      className={`z-99999 fixed left-0 top-0 w-full py-4 ${stickyMenu
         ? "bg-white shadow transition duration-100 dark:bg-black"
         : ""
         }`}
