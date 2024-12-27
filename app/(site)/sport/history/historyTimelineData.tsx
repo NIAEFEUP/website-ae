921286bd2ee4bb89953df1fdd8c359f85b612436
Eduarda <eduardagouveiamagno.com>
2024-12-27 03:26:52 +0000

    complete timeline

--- app/(site)/sport/history/historyTimelineData.tsx
+++ app/(site)/sport/history/historyTimelineData.tsx
@@ -3,14 +3,47 @@ import React from "react";
 export const historyTimelineData = [
   {
     title: "2024: Lorem",
-    content: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel velit tincidunt, commodo nisl id, rutrum nulla. Sed at consequat est. Donec ullamcorper blandit viverra. Vivamus eu lobortis purus. Sed convallis sem orci, nec tincidunt nibh ornare quis. Nullam eu turpis et nisl blandit iaculis. Morbi laoreet luctus dui, vel condimentum nisl maximus vitae.</p>,
+    content: (
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel
+        velit tincidunt, commodo nisl id, rutrum nulla. Sed at consequat est.
+        Donec ullamcorper blandit viverra. Vivamus eu lobortis purus. Sed
+        convallis sem orci, nec tincidunt nibh ornare quis. Nullam eu turpis et
+        nisl blandit iaculis. Morbi laoreet luctus dui, vel condimentum nisl
+        maximus vitae.
+      </p>
+    ),
   },
   {
     title: "2023: Lorem",
-    content: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel velit tincidunt, commodo nisl id, rutrum nulla. Sed at consequat est. Donec ullamcorper blandit viverra. Vivamus eu lobortis purus. Sed convallis sem orci, nec tincidunt nibh ornare quis. Nullam eu turpis et nisl blandit iaculis. Morbi laoreet luctus dui, vel condimentum nisl maximus vitae..</p>,
+    content: (
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel
+        velit tincidunt, commodo nisl id, rutrum nulla. Sed at consequat est.
+        Donec ullamcorper blandit viverra. Vivamus eu lobortis purus. Sed
+        convallis sem orci, nec tincidunt nibh ornare quis. Nullam eu turpis et
+        nisl blandit iaculis. Morbi laoreet luctus dui, vel condimentum nisl
+        maximus vitae.
+      </p>
+    ),
   },
   {
     title: "2022: Lorem",
-    content: <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel velit tincidunt, commodo nisl id, rutrum nulla. Sed at consequat est. Donec ullamcorper blandit viverra. Vivamus eu lobortis purus. Sed convallis sem orci, nec tincidunt nibh ornare quis. Nullam eu turpis et nisl blandit iaculis. Morbi laoreet luctus dui, vel condimentum nisl maximus vitae.</p>,
+    content: (
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vel
+          velit tincidunt, commodo nisl id, rutrum nulla. Sed at consequat est.
+          Donec ullamcorper blandit viverra. Vivamus eu lobortis purus. Sed
+          convallis sem orci, nec tincidunt nibh ornare quis. Nullam eu turpis
+          et nisl blandit iaculis. Morbi laoreet luctus dui, vel condimentum
+          nisl maximus vitae.
+        </p>
+      </div>
+    ),
+    images: [
+        "/images/landing/blog-04.png",
+        "/images/landing/blog-04.png",    
+      ],
   },
 ];
