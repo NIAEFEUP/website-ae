dc25e2993f36b2395a5755f16f96413c6476f87b
Rubem Neto <rubemviscard2635@gmail.com>
2025-01-27 15:55:27 +0000

    fix:blob token type fix

--- payload.config.ts
+++ payload.config.ts
@@ -83,7 +83,7 @@ export default buildConfig({
         },
       },
       // Token provided by Vercel once Blob storage is added to your Vercel project
-      token: process.env.BLOB_READ_WRITE_TOKEN,
+      token: process.env.BLOB_READ_WRITE_TOKEN || "",
     }),
   ],
   i18n: {
