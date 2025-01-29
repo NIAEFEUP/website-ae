61014f8a254daa4285df7699eecf396f378cf45b
Rubem Neto <rubemviscard2635@gmail.com>
2025-01-29 00:20:17 +0000

    fix: choice of features and optional description

--- app/(site)/landing/landingFeaturesData.tsx
+++ app/(site)/landing/landingFeaturesData.tsx
@@ -6,30 +6,37 @@ const landingFeaturesData: Feature[] = [
     id: 1,
     icon: <Building />,
     title: "Cedências de Espaço",
-    description: "",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
     href: "requests"
   },
-  {
+/*   {
     id: 2,
     icon: <HandPlatter />,
     title: "Serviços",
     description: "",
     href: "services"
+  }, */
+  {
+    id: 2,
+    icon: <HandPlatter />,
+    title: "Ementas",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
+    href: "menu"
   },
   {
     id: 3,
     icon: <Map />,
     title: "Espaços",
-    description: "",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In convallis tortor.",
     href: "student/spaces"
   },
-  {
+/*   {
     id: 4,
     icon: <HeartPulse />,
     title: "Apoio Social e Bem-Estar",
     description: "",
     href: "student/support"
-  },
+  }, */
 ];
 
 export default landingFeaturesData;
