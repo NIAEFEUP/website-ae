73c4b8affd7eb3a13b76ec7eb45c04c54878fa82
Rubem Neto <rubemviscard2635@gmail.com>
2025-01-29 15:10:46 +0000

    fix: state on shop

--- app/(site)/shop/page.tsx
+++ app/(site)/shop/page.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import ShopPageContent from "./pageContent";
 import { getProducts } from "@/actions/getShopProducts";
 
+export const revalidate = 10
+export const dynamicParams = true
+
 export default async function ShopPage() {
   const products = await getProducts();
 
