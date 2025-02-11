71cce17544b531f26c6b36b9c8d97cd37edc3286
Eduarda <eduardagouveiamagno.com>
2025-02-11 19:04:24 +0000

    horizontal_timeline

---
+++ app/(site)/sport/motorsport/timelineData.tsx
@@ -0,0 +1,40 @@
+const timelineData = [
+    {
+      title: "2024",
+      content: (
+        <ul className="list-disc pl-5">
+        <strong className="text-primary">Formula Students</strong>
+          <li> Vitória na competição de Concept Class na Suíça (FSCH)</li>
+          <li> Vitória na competição de Concept Class em Portugal (FSPT)</li>
+          <li> Segundo Lugar na Competição de Concept Class em Silverstone (FSUK)</li>
+          <br />
+          <strong className="text-primary">MotoStudents</strong>
+          <li>Preparação para a participação na 8ª edição da competição MotoStudent, focando-se na sustentabilidade e colaboração entre diferentes áreas de estudo para desenvolver um protótipo funcional.</li>
+        </ul>
+      ),
+    },
+    {
+      title: "2023",
+      content: (
+        <ul className="list-disc pl-5">
+        <strong className="text-primary">Formula Students</strong>
+          <li> Prémio Best EV Newcomer em Silverstone (FSUK)</li>
+          <li> Prémio FSG Best prepared car for scruteneering em Hockenheim (FSG)</li>
+          <br />
+        </ul>
+      ),
+    },
+    {
+      title: "2022",
+      content: (
+        <ul className="list-disc pl-5">
+        <strong className="text-primary">Formula Students</strong>
+          <li> Vitória na Competição de Concept Class em Silverstone (FSUK)</li>
+          <li> Segundo Lugar na Competição de Concept Class em Varano de' Melegari (Formula ATA)</li>
+        </ul>
+      ),
+    },
+  ];
+  
+  export default timelineData;
+  
