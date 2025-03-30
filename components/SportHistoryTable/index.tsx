edade0d4d4cbb8b59e18372960efba3c0b22ca11
Rubem Neto <rubemviscard2635@gmail.com>
2025-03-30 19:46:54 +0100

    fix:sport_history responsive

--- components/SportHistoryTable/index.tsx
+++ components/SportHistoryTable/index.tsx
@@ -8,16 +8,16 @@ interface TableData {
 
 const SportHistoryTable: React.FC<{ data: TableData[] }> = ({ data }) => {
   return (
-    <table className="min-w-full mt-4 mb-4 text-left text-sm border-separate border-spacing-0">
+    <table className="table-fixed mt-4 mb-4 text-left text-sm border-separate border-spacing-0">
       <thead>
         <tr>
-          <th className="px-4 py-2 font-medium border-b border-gray-300 dark:border-gray-600">
+          <th className="px-1 md:px-4 py-2 font-medium border-b border-gray-300 dark:border-gray-600">
             Competição
           </th>
-          <th className="px-4 py-2 font-medium border-b border-gray-300 dark:border-gray-600">
+          <th className="px-1 md:px-4 py-2 font-medium border-b border-gray-300 dark:border-gray-600">
             Modalidade
           </th>
-          <th className="px-4 py-2 font-medium border-b border-gray-300 dark:border-gray-600">
+          <th className="px-1 md:px-4 py-2 font-medium border-b border-gray-300 dark:border-gray-600">
             Classificação
           </th>
         </tr>
@@ -30,13 +30,13 @@ const SportHistoryTable: React.FC<{ data: TableData[] }> = ({ data }) => {
           return (
             <React.Fragment key={index}>
               <tr className="border-b border-gray-300 dark:border-gray-600">
-                <td className="px-4 py-2 border-r border-gray-300 dark:border-gray-600">
+                <td className="px-1 md:px-4 py-2 border-r border-gray-300 dark:border-gray-600">
                   {isFirstLabel ? <span className="font-semibold">{item.label}</span> : ""}
                 </td>
-                <td className="px-4 py-2 border-r border-gray-300 dark:border-gray-600">
+                <td className="px-1 md:px-4 py-2 border-r border-gray-300 dark:border-gray-600">
                   {item.value}
                 </td>
-                <td className="px-4 py-2">{item.classification}</td>
+                <td className="px-1 md:px-4 py-2">{item.classification}</td>
               </tr>
               {isLastOfLabel && index !== data.length - 1 && (
                 <tr>
